fd3b2fb63168686cb55cc8d53b504864a3ce80a8
Redomine <romankaev@gmail.com>
2022-11-11 12:56:35 +0300

    Свел получение параметров в одну функцию с заменой

--- lib/Redomine.py
+++ lib/Redomine.py
@@ -29,6 +29,43 @@ import os.path as op
 import clr
 
 
+def getSharedParameter (element, paraName, replaceName):
+    if element.Category.IsId(BuiltInCategory.OST_PipeCurves):
+        if paraName == 'ADSK_Наименование':
+            ElemTypeId = element.GetTypeId()
+            ElemType = doc.GetElement(ElemTypeId)
+            if ElemType.LookupParameter('ФОП_ВИС_Имя трубы из сегмента').AsInteger() == 1:
+                parameter = element.LookupParameter('Описание сегмента').AsString()
+                return parameter
+
+    report = paraName.split('_')[1]
+    replaceDef = isReplasing(replaceName)
+    name = paraName
+    if replaceDef:
+            name = replaceIsValid(element, paraName, replaceName)
+    try:
+        parameter = element.LookupParameter(name).AsString()
+        if parameter == None:
+            parameter = 'None'
+    except Exception:
+        #try:
+        ElemTypeId = element.GetTypeId()
+        ElemType = doc.GetElement(ElemTypeId)
+
+        if ElemType.LookupParameter(name) == None:
+            parameter = "None"
+        else:
+            parameter = ElemType.LookupParameter(name).AsString()
+
+
+    nullParas = ['ADSK_Завод-изготовитель', 'ADSK_Марка', 'ADSK_Код изделия']
+    if parameter == 'None' or parameter == None:
+        if paraName in nullParas:
+            parameter = ''
+        #except Exception:
+        #    print 'Невозможно получить параметр ' + report
+        #    sys.exit()
+    return parameter
 def make_col(category):
     col = FilteredElementCollector(doc) \
         .OfCategory(category) \
@@ -57,134 +94,30 @@ def replaceIsValid (element, paraName, replaceName):
 def get_ADSK_Izm(element):
     paraName = 'ADSK_Единица измерения'
     replaceName = 'ФОП_ВИС_Замена параметра_Единица измерения'
-    replaceDef = isReplasing(replaceName)
-    if replaceDef:
-            paraName = replaceIsValid(element, paraName, replaceName)
-    try:
-        ADSK_Izm = element.LookupParameter(paraName).AsString()
-    except Exception:
-        try:
-            ElemTypeId = element.GetTypeId()
-            ElemType = doc.GetElement(ElemTypeId)
-
-            if ElemType.LookupParameter(paraName) == None:
-                ADSK_Izm = "None"
-            else:
-                ADSK_Izm = ElemType.LookupParameter(paraName).AsString()
-        except Exception:
-            print 'Невозможно получить параметр единицы измерения'
-            sys.exit()
+    ADSK_Izm = getSharedParameter(element, paraName, replaceName)
     return ADSK_Izm
 
 def get_ADSK_Name(element):
     paraName = 'ADSK_Наименование'
     replaceName = 'ФОП_ВИС_Замена параметра_Наименование'
-    replaceDef = isReplasing(replaceName)
-    if replaceDef:
-            paraName = replaceIsValid(element, paraName, replaceName)
-    try:
-        if element.LookupParameter(paraName):
-            ADSK_Name = element.LookupParameter(paraName).AsString()
-            if ADSK_Name == None or ADSK_Name == "":
-                ADSK_Name = "None"
-        else:
-            ElemTypeId = element.GetTypeId()
-            ElemType = doc.GetElement(ElemTypeId)
-
-            if ElemType.LookupParameter(paraName) == None\
-                    or ElemType.LookupParameter(paraName).AsString() == None \
-                    or ElemType.LookupParameter(paraName).AsString() == "":
-                ADSK_Name = "None"
-            else:
-                ADSK_Name = ElemType.LookupParameter(paraName).AsString()
-    except Exception:
-        print 'Невозможно получить параметр наименования'
-        sys.exit()
-
-    if str(element.Category.Name) == 'Трубы':
-        ElemTypeId = element.GetTypeId()
-        ElemType = doc.GetElement(ElemTypeId)
-        if ElemType.LookupParameter('ФОП_ВИС_Имя трубы из сегмента'):
-            if ElemType.LookupParameter('ФОП_ВИС_Имя трубы из сегмента').AsInteger() == 1:
-                ADSK_Name = element.LookupParameter('Описание сегмента').AsString()
-
+    ADSK_Name = getSharedParameter(element, paraName, replaceName)
     return ADSK_Name
 
 def get_ADSK_Maker(element):
     paraName = 'ADSK_Завод-изготовитель'
     replaceName = 'ФОП_ВИС_Замена параметра_Завод-изготовитель'
-    replaceDef = isReplasing(replaceName)
-    if replaceDef:
-            paraName = replaceIsValid(element, paraName, replaceName)
-
-    try:
-        if element.LookupParameter(paraName):
-            ADSK_Maker = element.LookupParameter(paraName).AsString()
-            if ADSK_Maker == None or ADSK_Maker == "":
-                ADSK_Maker = ""
-        else:
-            ElemTypeId = element.GetTypeId()
-            ElemType = doc.GetElement(ElemTypeId)
-
-            if ElemType.LookupParameter(paraName) == None\
-                    or ElemType.LookupParameter(paraName).AsString() == None \
-                    or ElemType.LookupParameter(paraName).AsString() == "":
-                ADSK_Maker = ""
-            else:
-                ADSK_Maker = ElemType.LookupParameter(paraName).AsString()
-    except Exception:
-        print 'Невозможно получить параметр завода-изготовителя'
-        sys.exit()
-
+    ADSK_Maker = getSharedParameter(element, paraName, replaceName)
     return ADSK_Maker
 
 def get_ADSK_Mark(element):
     paraName = 'ADSK_Марка'
     replaceName = 'ФОП_ВИС_Замена параметра_Марка'
-    replaceDef = isReplasing(replaceName)
-    if replaceDef:
-            paraName = replaceIsValid(element, paraName, replaceName)
-
-    try:
-        if element.LookupParameter(paraName):
-            ADSK_Mark = element.LookupParameter(paraName).AsString()
-        else:
-            ElemTypeId = element.GetTypeId()
-            ElemType = doc.GetElement(ElemTypeId)
-
-            if ElemType.LookupParameter(paraName) == None \
-                    or ElemType.LookupParameter(paraName).AsString() == None \
-                    or ElemType.LookupParameter(paraName).AsString() == "":
-                ADSK_Mark = "None"
-            else:
-                ADSK_Mark = ElemType.LookupParameter(paraName).AsString()
-    except Exception:
-        print 'Невозможно получить параметр марки'
-        sys.exit()
-
+    ADSK_Mark = getSharedParameter(element, paraName, replaceName)
     return ADSK_Mark
 
 def get_ADSK_Code(element):
     paraName = 'ADSK_Код изделия'
     replaceName = 'ФОП_ВИС_Замена параметра_Код изделия'
-    replaceDef = isReplasing(replaceName)
-    if replaceDef:
-            paraName = replaceIsValid(element, paraName, replaceName)
-
-    if element.LookupParameter(paraName):
-        ADSK_Code = element.LookupParameter(paraName).AsString()
-    else:
-        ElemTypeId = element.GetTypeId()
-        ElemType = doc.GetElement(ElemTypeId)
-
-        if ElemType.LookupParameter(paraName) == None \
-                or ElemType.LookupParameter(paraName).AsString() == None \
-                or ElemType.LookupParameter(paraName).AsString() == "":
-            ADSK_Code = ""
-        else:
-            ADSK_Code = ElemType.LookupParameter(paraName).AsString()
+    ADSK_Code = getSharedParameter(element, paraName, replaceName)
     return ADSK_Code
-
-
-
 
