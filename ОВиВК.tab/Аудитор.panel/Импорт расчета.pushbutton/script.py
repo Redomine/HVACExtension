480edd86b5b90ced2bb656442f4a5d5b16805b3c
Mankaev_r <romankaev@gmail.com>
2025-02-07 13:19:02 +0300

    Обновление расчета, учет центров приборов а не их точек вставки

--- ОВиВК.tab/Аудитор.panel/Импорт расчета.pushbutton/script.py
+++ ОВиВК.tab/Аудитор.panel/Импорт расчета.pushbutton/script.py
@@ -108,8 +108,12 @@ def distance(point1, point2):
     """ Вычисляет расстояние между двумя точками в 3D пространстве """
     return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2 + (point1.z - point2.z)**2)
 
-def is_within_sphere(auditor_equipment, revit_coords, radius=1500):
+def is_within_sphere(auditor_equipment, revit_coords, radius=1000):
+
     """ Проверяет, входит ли точка auditor_equipment в сферу радиуса radius вокруг revit_coords """
+    # if(distance(auditor_equipment, revit_coords) <= radius):
+    #     print(distance(auditor_equipment, revit_coords))
+
     return distance(auditor_equipment, revit_coords) <= radius
 
 def extract_heating_device_description(file_path, reading_rules):
@@ -131,10 +135,10 @@ def extract_heating_device_description(file_path, reading_rules):
                     float(data[reading_rules.x_index].replace(',', '.')) * 1000,
                     float(data[reading_rules.y_index].replace(',', '.')) * 1000,
                     float(data[reading_rules.z_index].replace(',', '.')) * 1000,
-                    float(data[reading_rules.len_index].replace(',', '.'))/304.8,
+                    float(data[reading_rules.len_index].replace(',', '.')),
                     data[reading_rules.code_index],
-                    float(data[reading_rules.real_power_index])  * 10.764,
-                    float(data[reading_rules.nominal_power_index] ) * 10.764,
+                    float(data[reading_rules.real_power_index]),
+                    float(data[reading_rules.nominal_power_index]),
                     float(data[reading_rules.setting_index]),
                     data[reading_rules.maker_index],
                     data[reading_rules.full_name_index]
@@ -157,11 +161,24 @@ def get_elements_by_category(category):
     return col
 
 def insert_data(element, auditor_data):
-    element.SetParamValue('ADSK_Размер_Длина', auditor_data.len)
+    real_power_watts = UnitUtils.ConvertToInternalUnits(auditor_data.real_power, UnitTypeId.Watts)
+    len_meters = UnitUtils.ConvertToInternalUnits(auditor_data.len, UnitTypeId.Millimeters)
+
+    element.SetParamValue('ADSK_Размер_Длина', len_meters)
     element.SetParamValue('ADSK_Код изделия', auditor_data.code)
     element.SetParamValue('ADSK_Настройка', auditor_data.setting)
-    element.SetParamValue('ADSK_Тепловая мощность', auditor_data.real_power)
+    element.SetParamValue('ADSK_Тепловая мощность', real_power_watts)
 
+def get_bb_center(bb):
+    minPoint = bb.Min
+    maxPoint = bb.Max
+
+    centroid = XYZ(
+        (minPoint.X + maxPoint.X) / 2,
+        (minPoint.Y + maxPoint.Y) / 2,
+        (minPoint.Z + maxPoint.Z) / 2
+    )
+    return centroid
 
 @notification()
 @log_plugin(EXEC_PARAMS.command_name)
@@ -182,31 +199,50 @@ def script_execute(plugin_logger):
 
     with revit.Transaction("BIM: Импорт расчетов"):
         for element in equipment:
-            was_found = False
             xyz = element.Location.Point
+            bb = element.GetBoundingBox()
+            bb_center = get_bb_center(bb)
+            elem_height = 0
+            if element.IsExistsParam('ADSK_Размер_Высота'):
+                elem_height = element.GetParamValue('ADSK_Размер_Высота')
 
             revit_coords = RevitXYZmms(
-                convert_to_mms(xyz.X),
-                convert_to_mms(xyz.Y),
-                convert_to_mms(xyz.Z - element.GetParamValue(BuiltInParameter.INSTANCE_ELEVATION_PARAM))
+                convert_to_mms(bb_center.X),
+                convert_to_mms(bb_center.Y),
+                convert_to_mms(bb.Min.Z - element.GetParamValue(BuiltInParameter.INSTANCE_ELEVATION_PARAM))
             )
 
+            # if element.Id.IntegerValue == 2708661:
+            #     print('{} {} {}'.format(bb_center.X, bb_center.Y, bb.Min.Z))
+            #     print(elem_height)
             for auditor_data in ayditror_equipment:
                 if is_within_sphere(auditor_data, revit_coords):
                     family_name = element.Symbol.Family.Name
                     if 'Обр_ОП_Универсальный' in family_name:
-                        was_found = True
                         auditor_data.processed = True
                         insert_data(element, auditor_data)
-            if not was_found:
+                        break  # Переходим к следующему элементу в equipment
+            else:
                 not_found_revit.append(element.Id.IntegerValue)
 
         for ayditror_data in ayditror_equipment:
-            print(ayditror_data.processed)
+            if not ayditror_data.processed:
+                not_found_ayditor.append(ayditror_data)
 
-    if len(not_found_revit) > 0:
-        print('ID Элементов Revit, которые не были обработаны:')
-        print(not_found_revit)
+
+    # if len(not_found_revit) > 0:
+    #     print('ID Элементов Revit, которые не были обработаны:')
+    #     print(not_found_revit)
+    #
+    # if len(not_found_ayditor) > 0:
+    #     print('Оборудование аудитор, которое не было найдено в модели:')
+    #     for ayditror_data in not_found_ayditor:
+    #         print('Прибор х: {}, y: {}, z: {}, артикул: {}, мощность: {}'.format(
+    #             ayditror_data.x,
+    #             auditor_data.y,
+    #             ayditror_data.z,
+    #             auditor_data.code,
+    #             auditor_data.real_power))
 
 
 script_execute()
